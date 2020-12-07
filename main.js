5eac7b8f6d0f60c448791395229fe66eaa331d0f
Kamil Szoka <kpszoka@gmail.com>
2020-12-07 21:35:07 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const name = "Kamil";
+const siteName = "Koza Inc.";
+
+alert(`Witaj na ${siteName}! Mam na imię ${name} i będę Twoim przewodnikiem`);
+console.log(`Witaj na ${siteName}! Mam na imię ${name} i będę Twoim przewodnikiem`)
