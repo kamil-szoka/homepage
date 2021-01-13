2ba81b67b3055562361869d0aaf4f823f95aafc9
Kamil Szoka <kpszoka@gmail.com>
2021-01-13 22:24:06 +0100

    minor project fixes

--- main.js
+++ main.js
@@ -18,19 +18,6 @@ const greeting = (name, age) => {
 
 greeting("Andrzej", 758);
 
-// Deklaracja funkcji - pamiętaj, że jeżeli nie dasz w instrukcji funkcji słowa kluczowego return, to potem przy próbie przypisania wywołania funkcji do zmiennej dostaniesz undefined - funkcja wtedy niczego nie zwraca, wykonuje w środku obliczenia, ale nie ma zdefiniowanego tego co ta funkcja ma nam zwrócić i ewentualnie przekazać do jakiejś zmiennej
-
-// function greeting(name, age, result) {
-//   console.log(
-//     `Witaj na mojej stronie internetowej. Ja jestem ${name}, mam ${age} lat`
-//   );
-//   return result * 3;
-//   console.log(`Wynik to ${result}`);
-// }
-
-// const result = greeting("Andrzej", 758, 3);
-// console.log(result);
-
 const burger = document.querySelector(".hamburger");
 
 const myClick = () => {
